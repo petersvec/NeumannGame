6ff63ec6439f0a0a837f0c5ddba45d9d161676e7
Andrej Urbanek <andrejurbanek455@gmail.com>
2021-04-13 11:59:46 +0200

    finish update method in SpaceStation

--- game/src/buildings/SpaceStation.cpp
+++ game/src/buildings/SpaceStation.cpp
@@ -14,4 +14,21 @@ namespace game
 		IBuilding{ hp, type, texture, location, ironCost, copperCost, siliconCost, owner },
 		IObject{ hp, type, texture, location, owner }
 	{}
+
+	void SpaceStation::update(std::shared_ptr<engine::Map> map,
+							  engine::ObjectManager objMan,
+							  bool toUpdate,
+							  engine::UnitFactoryPtr unitFactory,
+							  game::PlayerState playerState1)
+	{
+		if (toUpdate)
+		{
+			makeWorker(unitFactory);
+		}
+	}
+
+	void SpaceStation::makeWorker(engine::UnitFactoryPtr unitFactory)
+	{
+		unitFactory.get()->create(game::ObjectType::Worker, getLocation(), getOwner());
+	}
 }
