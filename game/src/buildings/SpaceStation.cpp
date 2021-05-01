1ca5a88bf88f7d2629636d742d7a76537e7574f5
Andrej Urbanek <andrejurbanek455@gmail.com>
2021-05-01 20:01:55 +0200

    fix SpaceStation to return if it created new Worker unit. At the attempted creating it was skipping the move to the other player

--- game/src/buildings/SpaceStation.cpp
+++ game/src/buildings/SpaceStation.cpp
@@ -16,18 +16,19 @@ namespace game
 							   IObject{ hp, type, texture, location, owner }
 	{}
 
-	void SpaceStation::update(std::shared_ptr<engine::Map> map,
+	bool SpaceStation::update(std::shared_ptr<engine::Map> map,
 							  std::shared_ptr<engine::ObjectManager> objMan,
-							  bool toUpdate,
 							  PlayerState& playerState)
 	{
-		if (toUpdate && playerState.checkBalance(getIronCost(), getCopperCost(), getSiliconCost()))
+		if (playerState.checkBalance(getIronCost(), getCopperCost(), getSiliconCost()))
 		{
 			playerState.updatePlayerBalances(-getIronCost(), -getCopperCost(), -getSiliconCost());
 			auto xy = engine::GetNearestFreeLocation(getLocation(), objMan);
 			engine::TilePtr location = map->getTile(xy.first, xy.second);
 			build(location, objMan);
+			return true;
 		}
+		return false;
 	}
 
 	void SpaceStation::build(engine::TilePtr location, std::shared_ptr<engine::ObjectManager> OM)
